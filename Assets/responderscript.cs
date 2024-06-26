2bad6ae5df0df1331f17b5bbec44966c6700a0c8
juanpanpanyz <101480015+juanpanpanyz@users.noreply.github.com>
2024-06-26 10:43:12 -0300

    termino la clase

---
+++ Assets/responderscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class responderscript : MonoBehaviour
+{
+    public InputField inputNumeros;
+    public float[] numeros;
+    public int currentIndex = 0;
+    public Button button;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
