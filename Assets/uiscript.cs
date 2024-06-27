64840a2ce345313da57e87e27aae97c9422256fb
juanpanpanyz <101480015+juanpanpanyz@users.noreply.github.com>
2024-06-27 15:44:07 -0300

    funciona parcial

---
+++ Assets/uiscript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class uiscript : MonoBehaviour
+{
+    public InputField inputField;
+    public GameObject textBoxi;
+    public GameObject panelRespuesta;
+    public GameObject resultado;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Responder()
+    {
+        string inputText = inputField.text;
+
+        inputField.gameObject.SetActive(false);
+        textBoxi.SetActive(false);
+        panelRespuesta.SetActive(true);
+
+        if (Convert.ToInt32(inputText) == spawnscript.cantidad)
+        {
+
+        }
+    }
+}
