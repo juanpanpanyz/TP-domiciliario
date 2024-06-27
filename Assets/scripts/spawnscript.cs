f076c3c8cf85f7a58c4033ea074d0e4c0e1f5a9e
juanpanpanyz <101480015+juanpanpanyz@users.noreply.github.com>
2024-06-27 11:56:33 -0300

    Agregado de nuevas Escenas

--- Assets/scripts/spawnscript.cs
+++ Assets/scripts/spawnscript.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class spawnscript : MonoBehaviour
 {
-
+    public float minX = 1f;  // Valor mínimo en el eje X
+    public float maxX = 15f; // Valor máximo en el eje X
+    public float ySpawn = 2f; // Posición fija en el eje Y
     public GameObject[] prefabsToSpawn; // Array de prefabs disponibles para spawnear
     public Transform[] spawns; // Array con los spawners
     private GameObject prefabToSpawn; // El prefab que se spawnea actualmente
