05b8454e56c4bfa3b41076bbf31c32dbd29c87ea
juanpanpanyz <101480015+juanpanpanyz@users.noreply.github.com>
2024-06-27 11:34:18 -0300

    agrego rigidbody entre otros

--- Assets/spawnscript.cs
+++ Assets/spawnscript.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class spawnscript : MonoBehaviour
 {
-    public int currentIndex;
-    public float minX = 1f;  // Valor mínimo en el eje X
-    public float maxX = 15f; // Valor máximo en el eje X
-    public float ySpawn = 2f; // Posición fija en el eje Y
-    public GameObject[] prefabsToShoot; // Array de prefabs a disparar
+
+    public GameObject[] prefabsToSpawn; // Array de prefabs disponibles para spawnear
+    public Transform[] spawns; // Array con los spawners
+    private GameObject prefabToSpawn; // El prefab que se spawnea actualmente
+
+    public float spawnInterval = 0.5f; // Intervalo entre cada spawn (en segundos)
 
     void Start()
     {
-            int randomIndex = Random.Range(0, prefabsToShoot.Length);
-            GameObject prefabToInstantiate = prefabsToShoot[randomIndex];
-            float randomX = Random.Range(minX, maxX);
-            Vector3 spawnPosition = new Vector3(randomX, ySpawn, 0f);
-            Instantiate(prefabToInstantiate, spawnPosition, Quaternion.identity);
+        // Seleccionar aleatoriamente un prefab inicial
+        int randomIndex = Random.Range(0, prefabsToSpawn.Length);
+        prefabToSpawn = prefabsToSpawn[randomIndex];
+
+        // Llama a la función para spawnean el prefab inicialmente y repite con el intervalo especificado
+        InvokeRepeating("SpawnPrefab", 0f, spawnInterval);
     }
 
-    void Update()
+    void SpawnPrefab()
     {
+        // Calcular una posición aleatoria dentro del rango definido
+        float randomX = Random.Range(minX, maxX);
+        Vector3 spawnPosition = new Vector3(randomX, ySpawn, 0f);
 
+        // Instanciar el prefab seleccionado en la posición calculada
+        Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
     }
 }
