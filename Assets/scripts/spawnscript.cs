20085fc1d2afb482038e6eab8c06519167a53c27
juanpanpanyz <101480015+juanpanpanyz@users.noreply.github.com>
2024-06-27 13:47:18 -0300

    correcciones

--- Assets/scripts/spawnscript.cs
+++ Assets/scripts/spawnscript.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class spawnscript : MonoBehaviour
 {
     public float minX = 1f;  // Valor mínimo en el eje X
     public float maxX = 15f; // Valor máximo en el eje X
     public float ySpawn = 2f; // Posición fija en el eje Y
+    public float Delay = 5.0f;
     public GameObject[] prefabsToSpawn; // Array de prefabs disponibles para spawnear
     public Transform[] spawns; // Array con los spawners
     private GameObject prefabToSpawn; // El prefab que se spawnea actualmente
@@ -21,6 +23,8 @@ public class spawnscript : MonoBehaviour
 
         // Llama a la función para spawnean el prefab inicialmente y repite con el intervalo especificado
         InvokeRepeating("SpawnPrefab", 0f, spawnInterval);
+
+        Invoke("EndDelay", 5.0f);
     }
 
     void SpawnPrefab()
@@ -31,5 +35,9 @@ public class spawnscript : MonoBehaviour
 
         // Instanciar el prefab seleccionado en la posición calculada
         Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+    }
+    void EndDelay()
+    {
+        SceneManager.LoadScene("Respuesta");
     }
 }
